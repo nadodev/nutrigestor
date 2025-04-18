2b527131cfdc592af93172b348404f41feb7334e
unknown <bemmenos@gmail.com>
2025-04-17 21:46:47 -0300

    ajustado o nextconfig

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*']
+    }
+  },
 };
 
 export default nextConfig;
+
+
