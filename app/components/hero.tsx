8ea56ae7ab4078a6c51d69485f5fa9cde4e900f1
unknown <bemmenos@gmail.com>
2025-04-17 22:39:40 -0300

    ajustado zindex do botao

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -8,7 +8,7 @@ export function Hero() {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
-      className="relative  overflow-hidden"
+      className="relative  overflow-hidden pt-32"
       id="home"
     >
       <div className="absolute inset-0 bg-gradient-to-b from-background/0 to-background/80"></div>
--- app/components/whatsapp-button.tsx
+++ app/components/whatsapp-button.tsx
@@ -6,7 +6,7 @@ import { BsWhatsapp } from 'react-icons/bs';
 export function WhatsAppButton() {
   return (
     <Button
-      className="fixed bottom-8 right-8 rounded-full w-14 h-14 p-0 shadow-lg hover:shadow-xl transition-all duration-300 animate-bounce bg-[#25D366] hover:bg-[#25D366]/90"
+      className=" z-50 fixed bottom-8 right-8 rounded-full w-14 h-14 p-0 shadow-lg hover:shadow-xl transition-all duration-300 animate-bounce bg-[#25D366] hover:bg-[#25D366]/90"
       onClick={() => window.open('https://wa.me/5511999999999', '_blank')}
     >
       <div className="flex items-center justify-center">
